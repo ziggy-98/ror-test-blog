3962faf7f8a9bdd9d954a7e9e849dab2d8a3b26d
ziggy-98 <luke@reallyquite.com>
2020-08-04 09:26:32 +0100

    added crud methods for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,48 @@
 class ArticlesController < ApplicationController
 
-    def new()
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+
+        @article.destroy
+
+        redirect_to articles_path
 
     end
 
     def create
         @article = Article.new(article_params)
 
-        @article.save
-
-        redirect_to @article
-    end
-
-    def show
-        @article = Article.find(params[:id])
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
 
