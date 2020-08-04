8d4bf67d2784beea57fbf891d0291b44c9baf940
ziggy-98 <luke@reallyquite.com>
2020-08-04 13:38:06 +0100

    added basic authentication on certain routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+    http_basic_authenticate_with name: "dhh", password: "secret",
+    except: [:index, :show]
+
     def show
         @article = Article.find(params[:id])
     end
